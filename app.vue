92683a73c10015ce3033f6ef4dd55a2acc9c8cae
SD-LJW <jay8126@gmail.com>
2024-01-30 01:41:37 +0900

    Car list

--- app.vue
+++ app.vue
@@ -1,27 +1,10 @@
 <template>
-	<div>
-		<!-- <NuxtWelcome /> -->
+	<header
+		class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+	>
+		<a class="text-3xl font-mono text-black" href="">car-trader</a>
+	</header>
 
-		<!-- NAVBAR -->
-
-		<header
-			class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-		>
-			<a class="text-3xl font-mono text-black" href="">car-trader</a>
-		</header>
-		<!-- NAVBAR -->
-
-		<!-- HOME HEROM -->
-		<div
-			class="relative h-screen bg-no-repeat bg-cover bg-bottom bg-[url('https://cdn.pixabay.com/photo/2014/09/07/22/34/car-race-438467_1280.jpg')]"
-		>
-			<div class="z-10 absolute w-full h-full flex justify-center items-center">
-				<div class="text-center w-1/2">
-					<h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
-				</div>
-			</div>
-			<div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
-		</div>
-		<!-- HOME HEROM -->
-	</div>
+	<!-- CAR DETAIL PAGE -->
+	<!-- CAR DETAIL PAGE -->
 </template>
