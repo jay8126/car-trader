d2633e19f07467c959d521d201b7004ee8d6d0a5
SD-LJW <jay8126@gmail.com>
2024-01-30 21:51:23 +0900

    capter 12

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-	<header
-		class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-	>
-		<a class="text-3xl font-mono text-black" href="">car-trader</a>
-	</header>
-
-	<!-- CAR DETAIL PAGE -->
-	<!-- CAR DETAIL PAGE -->
+	<div>
+		<NuxtPage />
+	</div>
 </template>
