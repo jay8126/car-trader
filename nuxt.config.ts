c1c85edfda6f6a393696af686b478c98148708c6
SD-LJW <jay8126@gmail.com>
2024-02-01 18:35:21 +0900

    33

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/ui', '@nuxt/image'],
+	modules: ['@nuxt/ui', '@nuxt/image', '@vueuse/nuxt'],
 	devtools: { enabled: true },
 });
